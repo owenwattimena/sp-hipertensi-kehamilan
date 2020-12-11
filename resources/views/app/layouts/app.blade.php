9401b3e0e97db755459aed52c9a971c55f8ba3aa
owenwattimena <wentoxwtt@gmail.com>
2020-12-11 12:38:38 +0900

    revisi tampilan info

--- resources/views/app/layouts/app.blade.php
+++ resources/views/app/layouts/app.blade.php
@@ -10,7 +10,7 @@ $current_route = Route::currentRouteName();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha3/dist/css/bootstrap.min.css" >
     
     <link rel="stylesheet" href="{{ asset('assets/app/css/main.css') }}">
     
--- resources/views/app/pages/info_penyakit.blade.php
+++ resources/views/app/pages/info_penyakit.blade.php
@@ -2,46 +2,76 @@
 
 @section('main')
 <div class="jumbotron jumbotron-fluid mb-0 pt-5" style="background-color: #FBFBFB">
-    <div class="container pb-3">
-        <h1 class="text-center ">HIPERTENSI KEHAMILAN</h1>
+  <div class="pb-3">
+    <div class="row">
+      <div class="col-3">
+        <h4 class="text-center ">HIPERTENSI KEHAMILAN</h4>
         <P class="text-center fondamento">Kenali gejalanya, kenali penyakitnya</P>
-        <div class="row">
-            <div class="col-4">
-                <div class="list-group" id="list-tab" role="tablist">
-                    @foreach ($penyakit as $item)
-                        
-                    <a class="list-group-item list-group-item-action mb-3 {{($loop->first) ? 'active' : ''}}" id="list-home-list" data-toggle="list" href="#list-{{$item->kode}}" role="tab" aria-controls="home">
-                        <img src="{{asset('assets/app/img/unnamed.png')}}" width="40" height="40" class="d-inline-block align-top" alt="">
-                        {{$item->nama}}</a>
-                    @endforeach
-                    {{-- <a class="list-group-item list-group-item-action mb-3" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">
-                        <img src="{{asset('assets/app/img/unnamed.png')}}" width="40" height="40" class="d-inline-block align-top" alt="">
-                        Profile</a>
-                    <a class="list-group-item list-group-item-action mb-3" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">
-                        <img src="{{asset('assets/app/img/unnamed.png')}}" width="40" height="40" class="d-inline-block align-top" alt="">
-                        Messages</a> --}}
-                </div>
-            </div>
-            <div class="col-8">
-                <div class="tab-content" id="nav-tabContent">
-                    @foreach ($penyakit as $item)
-                        <div class="tab-pane fade show active" id="list-{{$item->kode}}" role="tabpanel" aria-labelledby="list-home-list">
-                            <h2>Definisi</h2>
-                            {!!$item->definisi!!}
-                            <h2>Gejala</h2>
-                            {!!$item->gejala!!}
-                            <h2>Pencegahan</h2>
-                            {!!$item->pencegahan!!}
-                            <h2>Pengobatan</h2>
-                            {!!$item->pengobatan!!}
-                        </div>
-                    @endforeach
-                    {{-- <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-                    <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-                    <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div> --}}
-                </div>
-            </div>
+        <div class="list-group" id="list-tab" role="tablist">
+          @foreach ($penyakit as $item)
+          
+          <a class="list-group-item list-group-item-action mb-3 {{($loop->first) ? 'active' : ''}}" id="list-{{$item->kode}}-list" data-toggle="list" href="#list-{{$item->kode}}" role="tab" aria-controls="{{$item->kode}}">
+            <img src="{{asset('assets/app/img/unnamed.png')}}" width="40" height="40" class="d-inline-block align-top" alt="">
+            {{$item->nama}}
+          </a>
+          @endforeach
+
         </div>
+      </div>
+      <div class="col-9">
+        <div class="container">
+          <div class="tab-content" id="nav-tabContent">
+            
+            @foreach ($penyakit as $item)
+            <div class="tab-pane fade show {{($loop->first) ? 'active' : ''}}" id="list-{{$item->kode}}" role="tabpanel" aria-labelledby="list-{{$item->kode}}-list">
+              <div class="accordion accordion-flush" id="accordionFlushExample">
+                
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingOne">
+                    <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                      Definisi
+                    </button>
+                  </h2>
+                  <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      {!!$item->definisi!!}
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                      Gejala
+                    </button>
+                  </h2>
+                  <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      {!!$item->gejala!!}
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="accordion-item">
+                  <h2 class="accordion-header" id="flush-headingThree">
+                    <button class="accordion-button collapsed" type="button" data-toggle="collapse" data-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                      Pencegahan
+                    </button>
+                  </h2>
+                  <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-parent="#accordionFlushExample">
+                    <div class="accordion-body">
+                      {!!$item->pencegahan!!}
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+            </div>
+            @endforeach
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
 @endsection
