7bf3e45d0c290a7cbaa52eff2fc3ac8d7a98cd3a
owenwattimena <wentoxwtt@gmail.com>
2021-08-09 23:10:05 +0900

    login validation

--- resources/views/app/layouts/app.blade.php
+++ resources/views/app/layouts/app.blade.php
@@ -15,6 +15,7 @@ $current_route = Route::currentRouteName();
     <link rel="stylesheet" href="{{ asset('assets/app/css/main.css') }}">
     
     <title>Sistem Pakar Diagnosa Hipertensi Ibu Hamil</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     @yield('style')
 </head>
 
@@ -59,7 +60,7 @@ $current_route = Route::currentRouteName();
 
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     
     @yield('script')
 </body>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,7 @@
 @extends('app.layouts.app')
-
+@section('style')
+    
+@endsection
 @section('main')
     <div class="container py-5">
         <div class="row justify-content-center">
@@ -31,15 +33,21 @@
                         <div class="form-group">
                             <div class="form-check pl-0">
                                 <label for="password" class="bt-caption">Password</label>
-                                <input type="password" class="rounded form-control form-control-lg" name="password" id="password"
+                                <div class="input-group">
+                                    <input type="password" class="rounded form-control form-control-lg" name="password" id="password"
                                     placeholder="Password" required>
+                                    <button class="btn btn-outline-secondary" id="btn-eye" type="button"> <i class="bi bi-eye"></i> </button>
+                                </div>
+                                <small class="text-danger validate-math visually-hidden">
+                                    Tidak boleh ada karakter matematika
+                                </small>
                                 <div class="invalid-feedback">
                                     Password tidak boleh kosong
                                 </div>
                             </div>
 
                         </div>
-                        <button class="btn btn-block bg-dark-purple rounded-0 btn-lg mt-5 text-white" type="submit">Login</button>
+                        <button class="btn w-100 bg-dark rounded-0 btn-lg mt-5 text-white" type="submit">Login</button>
                     </form>
                     <a href="{{url('/')}}" class="d-block text-center mt-3 bt-caption">&#8592; BERANDA</a>
                     <script>
@@ -67,4 +75,31 @@
             </div>
         </div>
     </div>
+@endsection
+@section('script')
+    <script>
+        let btn_eye = $('#btn-eye');
+        let input_password = $('#password');
+        btn_eye.click(function(){
+            var input_type = input_password.attr('type');
+            if(input_type == 'password')
+            {
+                input_password.attr('type', 'text')
+                $('#btn-eye i').removeClass('bi-eye')
+                $('#btn-eye i').addClass('bi-eye-slash')
+            }else{
+                input_password.attr('type', 'password')
+                $('#btn-eye i').addClass('bi-eye')
+                $('#btn-eye i').removeClass('bi-eye-slash')
+            }
+        })
+        $('#password').on('keyup', function(){
+            var str = $(this).val();
+            if(/^[a-zA-Z0-9_ ]*$/.test(str) == false) {
+                $('.validate-math').removeClass('visually-hidden')
+            }else{
+                $('.validate-math').addClass('visually-hidden')
+            }
+        });
+    </script>
 @endsection
